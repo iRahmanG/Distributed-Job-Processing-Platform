5ca3bbc2ce54ccc973b7c2d840047cd5b34d454c
iRahmanG <rahmanmr039@gmail.com>
2026-07-27 16:11:39 +0530

    chore: initialize distributed job platform project

---
+++ src/main/java/com/maksud/jobplatform/DistributedJobPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.maksud.jobplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+DistributedJobPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedJobPlatformApplication.class, args);
+	}
+
+}
