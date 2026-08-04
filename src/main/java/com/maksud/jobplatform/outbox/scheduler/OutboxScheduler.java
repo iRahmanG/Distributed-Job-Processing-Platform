01bc50446e480d34a2ea7d58f68381ad4155d04b
iRahmanG <rahmanmr039@gmail.com>
2026-08-04 15:07:01 +0530

    feat(outbox): implement scheduled outbox event publisher

--- src/main/java/com/maksud/jobplatform/outbox/scheduler/OutboxScheduler.java
+++ src/main/java/com/maksud/jobplatform/outbox/scheduler/OutboxScheduler.java
@@ -1,4 +1,19 @@
 package com.maksud.jobplatform.outbox.scheduler;
 
+import com.maksud.jobplatform.outbox.service.OutboxPublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class OutboxScheduler {
+
+    private final OutboxPublisherService outboxPublisherService;
+
+    @Scheduled(fixedDelayString = "${job-platform.outbox.publish-interval-ms}")
+    public void publishPendingEvents(){
+        outboxPublisherService.publishPendingEvent();
+    }
+
 }
