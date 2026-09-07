dedc9d90f1e0e937d1984f548b2bad1fe7acd67a
iRahmanG <rahmanmr039@gmail.com>
2026-09-08 00:27:22 +0530

    test: add job lifecycle unit test

---
+++ src/test/java/com/maksud/jobplatform/job/service/JobLifecycleServiceImplTest.java
@@ -0,0 +1,120 @@
+package com.maksud.jobplatform.job.service;
+
+import com.maksud.jobplatform.job.entity.Job;
+import com.maksud.jobplatform.job.entity.enums.JobStatus;
+import com.maksud.jobplatform.job.exception.InvalidJobStatusTransitionException;
+import com.maksud.jobplatform.job.repository.JobRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class JobLifecycleServiceImplTest {
+    @Mock
+    private JobRepository jobRepository;
+
+    @InjectMocks
+    private JobLifecycleServiceImpl jobLifecycleService;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(
+                jobLifecycleService,
+                "retryDelaySeconds",
+                30L
+        );
+    }
+
+    @Test
+    void shouldClaimQueuedJobSuccessfully(){
+        when(jobRepository.claimJob(
+                any(),
+                any(),
+                any(),
+                any()
+        )).thenReturn(1);
+        boolean result = jobLifecycleService.claimQueuedJob("job-123");
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnFalseJobWasAlreadyClaimed() {
+        when(jobRepository.claimJob(
+                any(),
+                any(),
+                any(),
+                any()
+        )).thenReturn(0);
+
+        boolean result = jobLifecycleService.claimQueuedJob("job-123");
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldMarkProcessingJobForRetry() {
+        Job job = Job.builder()
+                .jobId("job-123")
+                .status(JobStatus.PROCESSING)
+                .retryCount(1)
+                .build();
+
+        when(jobRepository.findById("job-123"))
+                .thenReturn(Optional.of(job));
+
+        when(jobRepository.markJobForRetry(
+                any(),
+                any(),
+                any(),
+                anyInt(),
+                any(),
+                any()
+        )).thenReturn(1);
+
+        jobLifecycleService.markRetry("job-123");
+
+        verify(jobRepository).markJobForRetry(
+                eq("job-123"),
+                eq(JobStatus.PROCESSING),
+                eq(JobStatus.RETRYING),
+                eq(2),
+                any(),
+                any()
+        );
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMarkingNonProcessingJobForRetry(){
+        Job job = Job.builder()
+                .jobId("job-123")
+                .status(JobStatus.COMPLETED)
+                .retryCount(1)
+                .build();
+
+        when(jobRepository.findById("job-123"))
+                .thenReturn(Optional.of(job));
+
+        assertThrows(
+                InvalidJobStatusTransitionException.class,
+                () -> jobLifecycleService.markRetry("job-123")
+        );
+
+        verify(jobRepository, never()).markJobForRetry(
+                any(),
+                any(),
+                any(),
+                anyInt(),
+                any(),
+                any()
+        );
+    }
+}
